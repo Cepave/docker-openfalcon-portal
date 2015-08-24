1d89f801eb6b3fc8c9219ef769fce4e7ea29b186
Minimum <s5550055@yahoo.com.tw>
2015-08-24 10:56:41 +0800

    Replace "bash" with "tail".

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ cp $CONFIGDIR/$CONFIGFILE $WORKDIR/frame/$CONFIGFILE
 virtualenv $WORKDIR/env
 pip install -r /home/portal/pip_requirements.txt -i http://pypi.douban.com/simple
 $WORKDIR/control restart
-/bin/bash
+$WORKDIR/control tail
